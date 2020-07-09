9a520ebdbf98a7c3494a7f8d34b949cdfbe63b5f
= <jsw2095@naver.com>
2020-07-09 23:42:02 +0900

    set middle ware

--- routes/admin.ts
+++ routes/admin.ts
@@ -1,15 +1,31 @@
 import * as express from 'express'
+import * as morgan from 'morgan';
+
+const testMiddleWare = (req: express.Request, res: express.Response, next: any) => {
+  console.log('first middleware');
+  next();
+};
+
+const testMiddleWare2 = (req: express.Request, res: express.Response, next: any) => {
+  console.log('second middleware');
+  next();
+};
+
+const loginRequired = (req: express.Request, res: express.Response, next: any) => {
+  
+}
 
 const adminRouter = express.Router();
 
-adminRouter.get('/', (req: express.Request, res: express.Response) => {
+adminRouter.get('/', testMiddleWare, testMiddleWare2, (req: express.Request, res: express.Response) => {
   res.send('admin 이후 url');
 });
 
 adminRouter.get('/products', (req: express.Request, res: express.Response) => {
+
+
   res.render('admin/products.html', {
     message: 'hello~',
-    online: '<h1>fdsfsd</h1>'
   });
 });
 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import adminRouter from '../routes/admin';
 import contactRouter from '../routes/contact';
 import * as nunjucks from 'nunjucks';
+import * as morgan from 'morgan';
 
 class App {
   public application : express.Application;
@@ -19,11 +20,19 @@ nunjucks.configure('templates', {
     express: app
 });
 
+// middleware setting
+app.use( morgan('dev') );
+
 app.get('/', (req: express.Request , res: express.Response) => {
   res.send('hello express');
 });
 
-app.use('/admin', adminRouter);
+const adminMiddleware = (req: express.Request, res: express.Response, next: any) => {
+  console.log('Admin Middleware');
+  next();
+}
+
+app.use('/admin', adminMiddleware, adminRouter);
 app.use('/contacts', contactRouter);
 
 app.listen(port, () => {
